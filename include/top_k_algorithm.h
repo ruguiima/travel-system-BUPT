603a006cb101ee6a6a5646199181bc41a5227ba2
lzc <93433@qq.com>
2025-05-28 15:59:21 +0800

    修改了topk算法，维护堆不使用<algorithm>头文件中的库函数，修改为自实现

--- include/top_k_algorithm.h
+++ include/top_k_algorithm.h
@@ -4,32 +4,75 @@
 #include <vector>
 #include <algorithm>
 
-
 template<typename T, typename criteria>
 class my_pq {
 private:
     std::vector<T> heap;
     criteria cmp;
+
+    // 自定义上浮操作（用于push）
+    void sift_up(size_t index) {
+        while (index > 0) {
+            size_t parent = (index - 1) / 2;
+            if (cmp(heap[parent], heap[index])) {
+                std::swap(heap[parent], heap[index]);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    // 自定义下沉操作（用于pop）
+    void sift_down(size_t index) {
+        size_t size = heap.size();
+        while (true) {
+            size_t left = 2 * index + 1;
+            size_t right = 2 * index + 2;
+            size_t largest = index;
+
+            if (left < size && cmp(heap[largest], heap[left])) {
+                largest = left;
+            }
+            if (right < size && cmp(heap[largest], heap[right])) {
+                largest = right;
+            }
+
+            if (largest != index) {
+                std::swap(heap[index], heap[largest]);
+                index = largest;
+            } else {
+                break;
+            }
+        }
+    }
+
 public:
     my_pq(criteria cmp) : cmp(cmp) {}
-    // 修改push和pop函数为自实现
-    void push(const T& item){
-        // 目前使用标准库维护堆，请修改为自实现
+
+    void push(const T& item) {
         heap.push_back(item);
-        std::push_heap(heap.begin(), heap.end(), cmp);
+        sift_up(heap.size() - 1);  // 上浮操作
     }
-    void pop(){
-        // 目前使用标准库维护堆，请修改为自实现
-        std::pop_heap(heap.begin(), heap.end(), cmp);
+
+    void pop() {
+        if (heap.empty()) return;
+        std::swap(heap.front(), heap.back());
         heap.pop_back();
+        if (!heap.empty()) {
+            sift_down(0);  // 下沉操作
+        }
     }
-    const T& top() const{
+
+    const T& top() const {
         return heap.front();
     }
-    bool empty() const{
+
+    bool empty() const {
         return heap.empty();
     }
 };
+
 // Top-K算法实现
 template<typename T, typename criteria>
 std::vector<T> getTopK(const std::vector<T>& items, int k, criteria cmp) {
@@ -57,7 +100,5 @@ std::vector<T> getTopK(const std::vector<T>& items, int k, criteria cmp) {
     std::reverse(result.begin(), result.end());
     return result;
 }
-
-
 
 #endif // TOPKALGORITHM_H
